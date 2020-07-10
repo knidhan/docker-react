0e7f58b72dd95f8e9300be41dc3ca6e0c68a7d2f
karuna nidhan <nidhan.karuna@ncs.com.sg>
2020-07-10 19:07:31 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="App-into">
+          HI There
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          "/learn react/i"
         </a>
       </header>
     </div>
